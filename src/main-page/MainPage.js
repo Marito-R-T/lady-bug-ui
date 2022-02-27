87061f033a6c70d44559c6546754d2ffb8b96d25
Marito-R-T <47235291+Marito-R-T@users.noreply.github.com>
2022-02-26 19:25:25 -0600

    Agregar Side Bar y Creación de Developer

---
+++ src/main-page/MainPage.js
@@ -0,0 +1,144 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import MuiAppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Outlet } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Badge from '@mui/material/Badge';
+import { Link } from 'react-router-dom';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import Avatar from '@mui/material/Avatar';
+import MuiDrawer from '@mui/material/Drawer';
+import { mainListItems, secondaryListItems } from './listItems';
+
+
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+        background: '#f0cdc2',
+        position: 'relative',
+        whiteSpace: 'nowrap',
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        boxSizing: 'border-box',
+        ...(!open && {
+            overflowX: 'hidden',
+            transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+            }),
+            width: theme.spacing(7),
+            [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9),
+            },
+        }),
+    },
+  }),
+);
+
+function MainPage() {
+    const [open, setOpen] = React.useState(true);
+    const toggleDrawer = () => {
+      setOpen(!open);
+    };
+    return (
+        <Box sx={{ display: 'flex' }}>
+        <AppBar position="absolute" color="neutral" open={open}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="dark"
+              aria-label="open drawer" 
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <Avatar src={process.env.PUBLIC_URL + "/LogoP.png"} />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="dark.main"
+              noWrap
+              sx={{ flexGrow: 1 }}
+            >
+              
+            </Typography>
+            <IconButton color="dark" component={Link} to="/">
+              <Typography color="dark.main">
+                <Badge badgeContent='Logout' color="primary">
+                  <LogoutIcon />
+                </Badge>
+              </Typography>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" color="neutral.main" open={open}>
+          <Toolbar
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              px: [1],
+            }}
+          >
+            <IconButton onClick={toggleDrawer}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+            {mainListItems}
+            <Divider sx={{ my: 1 }} />
+            {secondaryListItems}
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+            <Outlet/>
+        </Box>
+      </Box>
+  );
+}
+
+export default MainPage;
