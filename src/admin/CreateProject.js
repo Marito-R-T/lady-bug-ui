7a0dda638b99be350015a37327588f3968e95de4
SofiaQuintana <sofiaquintana2@gmail.com>
2022-03-27 23:11:16 -0600

    fix: fixed async issues

--- src/admin/CreateProject.js
+++ src/admin/CreateProject.js
@@ -26,18 +26,6 @@ const CreateProject = () => {
 
     const fetchProject = async (/*Name*/name, /*description*/desc, /*pm ID*/pmId, /*start Date*/sd, /*due Date*/dd) => {
         const auth = `${Cookies.get('tokenType')} ${Cookies.get('token')}`;
-        // const auth = (Cookies.get('tokenType') + ' ' + Cookies.get('token'));
-        // const requestOptions = {
-        // method: 'POST',
-        // headers: { 'Content-Type': 'application/json', 'Acces-Control-Allow-Origin': '*', 'Authorization': auth},
-        // body: JSON.stringify({
-        //     "name": name,
-        //     "description": desc,
-        //     "pmId": 4,
-        //     "startDate": sd,
-        //     "dueDate": dd
-        //   })
-        // };
         try {
             const response = await axios.post('https://ladybugger.herokuapp.com/admin/create-project', 
                 {
@@ -58,24 +46,12 @@ const CreateProject = () => {
         } catch(error) {
             console.error('Error:', error);
         }
-        
-        // await fetch('https://ladybugger.herokuapp.com/admin/create-project',requestOptions).then(res => res.json())
-        // .catch(error => console.error('Error:', error))
-        // .then(response => {
-        // console.log('Success:', response)
-        // navigate("#");
-        // });
     }
 
     const [items, setItems] = useState([]);
 
     const fetchItems = async () => {
         const auth = `${Cookies.get('tokenType')} ${Cookies.get('token')}`;
-        // const auth = (Cookies.get('tokenType') + ' ' + Cookies.get('token'));
-        // const requestOptions = {
-        //     method: 'GET',
-        //     headers: { 'Content-Type': 'application/json', 'Acces-Control-Allow-Origin': '*', 'Authorization': auth }
-        // };
         const response = await axios.get('https://ladybugger.herokuapp.com/admin/devs-list', 
             {
                 headers: {
